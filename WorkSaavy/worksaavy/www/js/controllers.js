bb479f5582792031fb267c7d234ccd061483e402
unknown <he282@DMUEVENT005012W.LEC-ADMIN.dmu.ac.uk>
2015-11-10 17:50:25 +0000

    webpage full

---
+++ WorkSaavy/worksaavy/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('workSaavyCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('tasksCtrl', function($scope) {
+
+})
+   
+.controller('task-1Ctrl', function($scope) {
+
+})
+   
+.controller('task-2Ctrl', function($scope) {
+
+})
+ 
